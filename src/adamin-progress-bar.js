642f236b80cfda74e05ae3798fdf0601900a4693
Adam L <git@adamin.net>
2012-08-18 13:00:19 -0700

    Initial commit

---
+++ src/adamin-progress-bar.js
@@ -0,0 +1,28 @@
+/*
+ * adamin-progress-bar
+ * https://github.com/pensive612/Adamin-Progress-Bar
+ *
+ * Copyright (c) 2012 Adam L.
+ * Licensed under the MIT, GPL licenses.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+      $(this).html('awesome');
+    });
+  };
+
+  // Static method.
+  $.awesome = function() {
+    return 'awesome';
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    return elem.textContent.indexOf('awesome') >= 0;
+  };
+
+}(jQuery));
